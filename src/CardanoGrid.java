c5f927cd74ad2768f455fdec0ae32e7bf002ee56
Kirill Apasov <kirill.apasov@yandex.ru>
2024-03-14 10:46:18 +0300

    Create Method for initial matrix

--- src/CardanoGrid.java
+++ src/CardanoGrid.java
@@ -1,6 +1,9 @@
 import java.util.Random;
 
 public class CardanoGrid {
+     private int[][] matrix = generateMatrix(8, 8);
+     private int[] keys = {4, 11, 17, 26, 7, 16, 21, 31, 36, 41, 43, 50, 40, 45, 47, 64};
+
     public String parser(String input){
         input = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         if(input.length() > 64){
@@ -16,5 +19,15 @@ public class CardanoGrid {
         Random random = new Random();
         char randomChar = (char) (random.nextInt(26) + 'a');
         return randomChar;
+    }
+    private int[][] generateMatrix(int rows, int cols) {
+        int[][] matrix = new int[rows][cols];
+        int counter = 1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = counter++;
+            }
+        }
+        return matrix;
     }
 }
