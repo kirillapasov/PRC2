62161f92f2408b5e5214f46025b1ebb81a2283ac
Kirill Apasov <kirill.apasov@yandex.ru>
2024-03-15 11:31:43 +0300

    Create two methods for Matrix operations

--- src/CardanoGrid.java
+++ src/CardanoGrid.java
@@ -28,6 +28,16 @@ public class CardanoGrid {
 
         return matrix;
     }
+    private int[][] createMatrix() {
+        int[][] matrix = new int[8][8];
+        int value = 1;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                matrix[i][j] = value++;
+            }
+        }
+        return matrix;
+    }
 
 
     private char generateRandomChar() {
@@ -35,12 +45,31 @@ public class CardanoGrid {
         char randomChar = (char) (random.nextInt(26) + 'a');
         return randomChar;
     }
+    private int[][] rotateMatrix(int[][] matrix) {
+        int[][] rotatedMatrix = new int[8][8];
+        // Поворот матрицы
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                rotatedMatrix[j][8 - 1 - i] = matrix[i][j];
+            }
+        }
+        return rotatedMatrix;
+    }
 
 
-    public void printMatrix(char[][] matrix) {
+    public static void printMatrix(char[][] matrix) {
         for (char[] row : matrix) {
             for (char cell : row) {
                 System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
         }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        CardanoGrid c = new CardanoGrid();
-        String mes = c.parser("GOVNOPENISYAYCAADKSHDIMAPEENIS");
-        char[][] ch1Arr = c.parseStringToMatrix(mes);
-        c.printMatrix(ch1Arr);
-        c.rotateMatrix(ch1Arr);
-        System.out.println("???????????????????????????");
-        c.printMatrix(ch1Arr);
-
 
         }
     }
